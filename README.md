eb9ed211ba073210dd9863f7e188a448167cfa42
henry <1151071+HenrYxZ@users.noreply.github.com>
2023-06-03 12:08:13 -0400

    refactor: abstract parsing of OBJ through another class

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# sombra-engine
+# sombra-engine 
+
+Game Engine made in Python on top of the pyglet framework (work in progress).
+
+## Planned features
+
+- 3D models through OBJ format
+- OpenGL Renderer and GLSL shaders
+- Pathfinding
+- Rigid body physics
+- Particle systems
+- Terrain rendering
+- UI with shaders support
--- sombra_engine/models/obj.py
+++ sombra_engine/models/obj.py
@@ -1,3 +1,5 @@
+from typing import TextIO
+
 from pyglet.math import Vec3
 from pyglet.graphics import Batch, Group
 from pyglet.graphics.shader import ShaderProgram
@@ -7,44 +9,67 @@ from sombra_engine.models import Mesh, Model
 from sombra_engine.primitives import Vertex
 
 
-def set_triangle(args, params):
-    values = args[1].split('/')
-    if len(values) == 1:
-        params['indices'] += args[1:]
-    elif len(values) == 2:
-        if '//' in args[1]:
-            vertices = params['vertices']
-            normals = params['normals']
-            for data in args[1:]:
-                values = data.split('/')
-                vert_id = int(values[0])
-                vertex = vertices[vert_id - 1]
-                params['indices'].append(vert_id)
-                normal_idx = int(values[2]) - 1
-                vertex.normal = normals[normal_idx]
-        else:
-            vertices = params['vertices']
-            tex_coords = params['tex_coords']
-            for data in args[1:]:
-                values = data.split('/')
-                vert_id = int(values[0])
-                vertex = vertices[vert_id - 1]
-                params['indices'].append(vert_id)
-                tex_coords_idx = int(values[1]) - 1
+class OBJParser:
+    def __init__(self):
+        self.meshes_data = []
+        self.current_mesh_data = {}
+
+    def set_name(self, name: str):
+        # If we already have a current mesh data we append it
+        if self.current_mesh_data.keys():
+            self.meshes_data.append(self.current_mesh_data)
+        self.current_mesh_data = {'name': name}
+
+    def set_vertex(self, args: list[str]):
+        if 'vertices' not in self.current_mesh_data:
+            self.current_mesh_data['vertices'] = []
+
+        positions = map(float, args)
+        new_vert = Vertex(
+            len(self.current_mesh_data['vertices']) + 1,
+            position=Vec3(*positions)
+        )
+        self.current_mesh_data['vertices'].append(new_vert)
+
+    def set_face(self, args: list[str]):
+        """
+        Set a new face by reading the given arguments and stores it in the
+        current mesh data dictionary. Will assume each element will always have
+        two '/'.
+
+        Args:
+            args: the arguments of the set face command as a list,
+                for ex. ['6/5/1', '7/3/2', '8/6/3']
+        """
+        vertices = self.current_mesh_data['vertices']
+        normals = self.current_mesh_data['normals']
+        tex_coords = self.current_mesh_data['tex_coords']
+
+        for indices_str in args:
+            # Split by '/' and transform the values to int
+            values_str = indices_str.split('/')
+
+            idx = int(values_str[0]) - 1
+            self.current_mesh_data['indices'].append(idx)
+            vertex = vertices[idx]
+            if values_str[1]:
+                tex_coords_idx = int(values_str[1]) - 1
                 vertex.tex_coords = tex_coords[tex_coords_idx]
-    else:
-        vertices = params['vertices']
-        normals = params['normals']
-        tex_coords = params['tex_coords']
-        for data in args[1:]:
-            values = data.split('/')
-            vert_id = int(values[0])
-            vertex = vertices[vert_id - 1]
-            params['indices'].append(vert_id)
-            tex_coords_idx = int(values[1]) - 1
-            vertex.tex_coords = tex_coords[tex_coords_idx]
-            normal_idx = int(values[2]) - 1
-            vertex.normal = normals[normal_idx]
+            if values_str[2]:
+                normal_idx = int(values_str[2]) - 1
+                vertex.normal = normals[normal_idx]
+
+    def parse(self, file: TextIO):
+        for line in file:
+            if not line or line[0] == '#':
+                continue
+            args = line.split(' ')
+            if args[0] == 'o':
+                self.set_name(args[1])
+            elif args[0] == 'v':
+                self.set_vertex(args[1:])
+            elif args[0] == 'f':
+                self.set_face(args[1:])
 
 
 class OBJLoader:
@@ -53,47 +78,25 @@ class OBJLoader:
         filename: str, name: str, program: ShaderProgram, batch: Batch = None,
         group: Group = None
     ):
-        meshes = []
-        current_mesh_params = {}
+        obj_parser = OBJParser()
         # Parse the file
         with open(filename) as f:
-            for line in f:
-                if not line or line[0] == '#':
-                    continue
-                args = line.split(' ')
-                if args[0] == 'o':
-                    # set name
-                    if current_mesh_params.keys():
-                        meshes.append(current_mesh_params)
-                    current_mesh_params = {}
-                    current_mesh_params['name'] = args[1]
-                elif args[0] == 'v':
-                    # set vertex
-                    if not 'vertices' in current_mesh_params:
-                        current_mesh_params['vertices'] = []
-                    else:
-                        new_vert = Vertex(
-                            len(current_mesh_params['vertices']) + 1,
-                            position=Vec3(*args[1:])
-                        )
-                        current_mesh_params['vertices'].append(new_vert)
-                elif args[0] == 'f':
-                    # set triangle
-                    set_triangle(args, current_mesh_params)
-
+            obj_parser.parse(f)
 
         # Now create the Model
-        for i, m in enumerate(meshes):
+        meshes = []
+        for mesh_data in obj_parser.meshes_data:
             # Create Mesh
-            meshes[i] = Mesh(
-                m['name'],
-                m['vertices'],
-                m['indices'],
-                vertex_groups=m['vertex_groups'],
-                materials=m['materials'],
+            new_mesh = Mesh(
+                mesh_data['name'],
+                mesh_data['vertices'],
+                mesh_data['indices'],
+                vertex_groups=mesh_data['vertex_groups'],
+                materials=mesh_data['materials'],
                 batch=batch,
                 group=group,
                 program=program
             )
+            meshes.append(new_mesh)
         model = Model(name, meshes)
         return model
