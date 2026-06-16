046700bef610bd6c03841225ad8ba5d3b418fbd8
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-06-16 16:12:09 -0400

    refactor: fix type hints in some constructors

--- sombra_engine/graphics/material_group.py
+++ sombra_engine/graphics/material_group.py
@@ -10,8 +10,12 @@ from sombra_engine import utils
 
 class MaterialGroup(Group):
     def __init__(
-        self, material: Material, program: ShaderProgram, matrix: Mat4,
-        order: int = 0, parent: Group = None
+        self,
+        material: Material,
+        program: ShaderProgram,
+        matrix: Mat4 = Mat4(),
+        order: int = 0,
+        parent: Group | None = None
     ):
         super().__init__(order, parent)
         self.program = program
