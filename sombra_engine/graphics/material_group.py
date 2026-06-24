5b8bb268a2e39c66038dabdac1e8f76e4bd4bdfc
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-06-24 17:00:13 -0400

    fix: use set_textures in MaterialGroup, uniforms are correctly set

--- sombra_engine/graphics/material_group.py
+++ sombra_engine/graphics/material_group.py
@@ -22,37 +22,40 @@ class MaterialGroup(ShaderGroup):
         self.program = program
         self.material = material
         self.matrix = matrix
+        textures = {}
 
         # Set ambient map
         self.ambient_map = material.ambient_map
         if self.ambient_map is None:
             self.ambient_map = create_white_tex()
-        self.set_texture(self.ambient_map, 0)
+        textures['ambient_map'] = self.ambient_map
 
         # Set diffuse map
         self.diffuse_map = material.diffuse_map
         if self.diffuse_map is None:
             self.diffuse_map = create_gray_tex()
-        self.set_texture(self.diffuse_map, 1)
+        textures['diffuse_map'] = self.diffuse_map
 
 
         # Set specular map
         self.specular_map = material.specular_map
         if self.specular_map is None:
             self.specular_map = create_black_tex()
-        self.set_texture(self.specular_map, 2)
+        textures['specular_map'] = self.specular_map
 
         # Set bump map
         if self.material.has_bump_map:
             self.bump_map = material.bump_map
             if self.bump_map is None:
                 self.bump_map = create_black_tex()
-            self.set_texture(self.bump_map, 3)
+            textures['bump_map'] = self.bump_map
         elif self.material.has_normal_map:
             self.normal_map = material.normal_map
             if self.normal_map is None:
                 self.normal_map = create_blue_tex()
-            self.set_texture(self.normal_map, 3)
+            textures['bump_map'] = self.normal_map
+
+        self.set_textures(textures, program)
 
         uniforms = self.get_uniforms()
         self.set_shader_uniforms(program, uniforms)
@@ -62,14 +65,14 @@ class MaterialGroup(ShaderGroup):
         uniforms = {}
         # Explicitly assign sampler uniforms to their texture units
         # (Required for GLSL 4.10 Core)
-        if 'ambient_map' in self.program._uniforms:
-            uniforms['ambient_map'] = 0
-        if 'diffuse_map' in self.program._uniforms:
-            uniforms['diffuse_map'] = 1
-        if 'specular_map' in self.program._uniforms:
-            uniforms['specular_map'] = 2
-        if 'bump_map' in self.program._uniforms:
-            uniforms['bump_map'] = 3
+        # if 'ambient_map' in self.program._uniforms:
+        #     uniforms['ambient_map'] = 0
+        # if 'diffuse_map' in self.program._uniforms:
+        #     uniforms['diffuse_map'] = 1
+        # if 'specular_map' in self.program._uniforms:
+        #     uniforms['specular_map'] = 2
+        # if 'bump_map' in self.program._uniforms:
+        #     uniforms['bump_map'] = 3
 
         if 'material.ambient' in self.program._uniforms:
             uniforms['material.ambient'] = self.material.ambient
