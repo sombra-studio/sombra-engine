a83e4f96db37abe8c353a7145b32a493fe796007
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-07-03 15:48:16 -0400

    fix: change Texture type for pyglet 3.0

--- sombra_engine/utils.py
+++ sombra_engine/utils.py
@@ -2,21 +2,22 @@ import math
 import numpy as np
 import pyglet
 
-def create_color_tex(color: tuple[int, int, int, int]) -> pyglet.image.Texture:
+
+def create_color_tex(color: tuple[int, int, int, int]) -> pyglet.graphics.Texture:
     color_pattern = pyglet.image.SolidColorImagePattern(color)
     img = color_pattern.create_image(16, 16)
     return img.get_texture()
 
-def create_black_tex() -> pyglet.image.Texture:
+def create_black_tex() -> pyglet.graphics.Texture:
     return create_color_tex((0, 0, 0, 255))
 
-def create_blue_tex() -> pyglet.image.Texture:
+def create_blue_tex() -> pyglet.graphics.Texture:
     return create_color_tex((0, 0, 255, 255))
 
-def create_white_tex() -> pyglet.image.Texture:
+def create_white_tex() -> pyglet.graphics.Texture:
     return create_color_tex((255, 255, 255, 255))
 
-def create_gray_tex() -> pyglet.image.Texture:
+def create_gray_tex() -> pyglet.graphics.Texture:
     return create_color_tex((123, 123, 123, 255))
 
 
