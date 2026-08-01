bed67c163a2cba9d6cc2483c5f422611ea85bb9a
henry <1151071+HenrYxZ@users.noreply.github.com>
2026-07-31 22:08:37 -0400

    docs: update README with Skeletal Animations

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
-# sombra-engine 
+# Sombra Engine 
 
-Game Engine made in Python on top of the pyglet framework (work in progress).
+Small 3D Game Engine made in Python on top of the pyglet framework (work in 
+progress).
+
+The idea of this engine is:
+- to be simple
+- to run on lower end PCs 
 
 ## Planned features
 
-- 3D models through glTF format (and limited support for OBJ)
-- OpenGL Renderer and GLSL shaders
-- Skeletal animations
-- Shadows
-- Pathfinding
-- Rigid body physics
-- Particle systems
-- Terrain rendering
-- UI library with shaders support (through [pudu-ui](https://github.com/sombra-studio/pudu-ui))
+- [x] UI library with shaders support (through [pudu-ui](https://github.com/sombra-studio/pudu-ui))
+- [x] 3D models through glTF format (and limited support for OBJ)
+- [x] OpenGL Renderer and GLSL shaders
+- [x] Skeletal animations
+- [ ] Shadows
+- [ ] Rigid body physics
+- [ ] Terrain rendering
+- [ ] Foliage
+- [ ] Particle systems
+- [ ] Water
+- [ ] Smoke, Fire, GFX
+- [ ] Pathfinding
+
 
 ![screenshot](https://raw.githubusercontent.com/sombra-studio/sombra-engine/refs/heads/main/docs/screenshot.png)
-
-You can get the OBJ model of Yoda from [here](https://graphics.cs.utah.edu/courses/cs6610/spring2022/prj04/yoda.zip)
 
 ## Installing
 
@@ -28,53 +35,44 @@ You can get it with pip:
 ## Example
 
 ```py
-import pyglet
 from pyglet.math import Vec3
+import pyglet
 
 
-from sombra_engine.app import App
-from sombra_engine.models.obj import OBJLoader
-from sombra_engine.scene import Scene
+from sombra_engine.models import SkeletalMesh
+from sombra_engine.models.gltf import GLTFLoader
+from sombra_engine import App
 
+
+pyglet.resource.path.append("./tests/data/")
+pyglet.resource.reindex()
 
 app = App(is_debug=True)
-model = None
-shader_program = None
-
-
-def update(dt):
-    global shader_program
-    if 'eye' in shader_program._uniforms:
-        shader_program['eye'] = app.camera.position
-    app.update(dt)
-
-
-def main():
-    global model, shader_program
-    model = OBJLoader.load(
-        filename="yoda.obj",
-        name="model",
-        batch=app.batch
-    )
-    scene = Scene()
-    scene.create_light(Vec3(10.0, 8.0, 0.0), Vec3(1.0, 1.0, 1.0))
-
-    program = model.meshes[0].program
-    shader_program = program
-    if 'light.position' in program._uniforms:
-        program['light.position'] = scene.lights[0].position
-    if 'light.color' in program._uniforms:
-        program['light.color'] = scene.lights[0].color
-    if 'eye' in program._uniforms:
-        program['eye'] = app.camera.position
-
-    app.add_model(model)
-
-    pyglet.clock.schedule(update)
-    pyglet.app.run(0)
 
 
 if __name__ == '__main__':
-    main()
+    batch = app.batch
+    scenes, skeletons, animations = GLTFLoader.load(
+        filename='CesiumMan.glb',
+        batch=batch
+    )
+
+    scene = scenes[0]
+    mesh = scene.meshes[0]
+    if isinstance(mesh, SkeletalMesh):
+        if mesh.animations:
+            # animations is a dict[str, Animation] where the keys are the 
+            # animation name. So this is just setting the first animation it 
+            # finds
+            animation_name = [k for k in mesh.animations.keys()][0]
+            mesh.set_animation(animation_name)
+
+    light_pos = Vec3(10.0, 8.0, 0.0)
+    light_color = Vec3(1.0, 1.0, 1.0)
+    scene.create_light(light_pos, light_color)
+    app.set_scene(scene)
+
+    pyglet.clock.schedule(app.update)
+    app.run(0)
 
 ```
--- main.py
+++ main.py
@@ -1,89 +1,36 @@
-from pyglet.gl import *
-from pyglet.graphics.shader import Shader, ShaderProgram
 from pyglet.math import Vec3
-from pyglet.window import key
 import pyglet
 
 
-from sombra_engine.camera import FPSCamera
-from sombra_engine.debug.gizmo import Gizmo
-from sombra_engine.models import Model, Wireframe
-from sombra_engine.models.obj import OBJLoader
-from sombra_engine.scene import Scene
+from sombra_engine.models import SkeletalMesh
+from sombra_engine.models.gltf import GLTFLoader
+from sombra_engine import App
 
 
-window = pyglet.window.Window(caption="Sombra Engine")
-camera = FPSCamera(
-    window, position=Vec3(0.0, 0.0, 15.0), pitch=90, yaw=-90
-)
-batch = pyglet.graphics.Batch()
-model: Model | None = None
-wf: Wireframe | None = None
-gizmo = Gizmo(size=10.0, batch=batch)
+pyglet.resource.path.append("./tests/data/")
+pyglet.resource.reindex()
 
-
-# def on_key_press(symbol, mod):
-#     if symbol == key.P:
-#         pyglet.image.get_buffer_manager().get_color_buffer().save(
-#             'docs/screenshot.png'
-#         )
-
-
-@window.event
-def on_draw():
-    window.clear()
-    glEnable(GL_CULL_FACE)
-    glEnable(GL_DEPTH_TEST)
-    glDepthFunc(GL_LESS)
-    batch.draw()
-
-
-def update(dt: float):
-    mesh = model.meshes[0]
-    mesh.rotate_y(dt / 3)
-    mesh.update(dt)
-
-
-def main():
-    global model, wf
-    with open('sombra_engine/shaders/default.vert') as f:
-       vert_shader = Shader(f.read(), 'vertex')
-    # with open('sombra_engine/shaders/blinn_barycentric.frag') as f:
-    # with open('sombra_engine/shaders/blinn.frag') as f:
-    # with open('sombra_engine/shaders/lambert.frag') as f:
-    # with open('sombra_engine/shaders/solid.frag') as f:
-    # with open('sombra_engine/shaders/normals.frag') as f:
-    # with open('sombra_engine/shaders/normals_from_bump.frag') as f:
-    # with open('sombra_engine/shaders/specular.frag') as f:
-    #     frag_shader = Shader(f.read(), 'fragment')
-    # program = ShaderProgram(vert_shader, frag_shader)
-
-    scene = Scene()
-    scene.create_light(Vec3(100.0, 150.0, -7.0), Vec3(1.0, 1.0, 1.0))
-
-    # model_group = pyglet.graphics.Group()
-    # model_group.visible = False
-    model = OBJLoader.load(
-        # "tests/data/ancient_house.obj", "House",
-        "tests/data/yoda/yoda.obj", "Yoda",
-        # scale=0.001,
-        # group=model_group,
-        # program=program,
-        batch=batch
-    )
-    program = model.meshes[0].program
-    program['light.position'] = scene.lights[0].position
-    program['light.color'] = scene.lights[0].color
-
-    program['eye'] = camera.position
-    print(f"object {model.name} loaded")
-    wf = Wireframe(model.meshes[0], vert_shader, batch=batch)
-
-    # window.push_handlers(on_key_press)
-
-    pyglet.clock.schedule_interval(update, 1 / 60)
-    pyglet.app.run()
+app = App(is_debug=True)
 
 
 if __name__ == '__main__':
-    main()
+    batch = app.batch
+    scenes, skeletons, animations = GLTFLoader.load(
+        filename='CesiumMan.glb',
+        batch=batch
+    )
+
+    scene = scenes[0]
+    mesh = scene.meshes[0]
+    if isinstance(mesh, SkeletalMesh):
+        if mesh.animations:
+            animation_name = [k for k in mesh.animations.keys()][0]
+            mesh.set_animation(animation_name)
+
+    light_pos = Vec3(10.0, 8.0, 0.0)
+    light_color = Vec3(1.0, 1.0, 1.0)
+    scene.create_light(light_pos, light_color)
+    app.set_scene(scene)
+
+    pyglet.clock.schedule(app.update)
+    app.run(0)
